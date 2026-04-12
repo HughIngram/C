2c016ce12662a1a8d95fb4829412b79d1d8a9ffa
Hugh Ingram <hin@medicuja.com>
2026-04-12 17:14:08 +0200

    chapter 1

---
+++ k-and-r/chapter-1/1_1_hello-world.c
@@ -0,0 +1,10 @@
+//
+// Created by Hugh Ingram on 12.04.26.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ k-and-r/chapter-1/1_3-temperature.c
@@ -0,0 +1,17 @@
+// C=(5/9)(F-32)
+
+#include <stdio.h>
+
+int main() {
+    printf("Farenheit\t|\tCelcius\n");
+    float lower = 0;
+    float upper = 300;
+    float step = 20;
+
+    float fahr = lower;
+    while (fahr <= upper) {
+        float celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f\t\t\t|\t%3.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
