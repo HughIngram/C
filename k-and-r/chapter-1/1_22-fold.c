421e0ae00d0490bc3671301eac9d6586a8326766
Hugh Ingram <hin@medicuja.com>
2026-04-19 23:36:37 +0200

    chapter 1

---
+++ k-and-r/chapter-1/1_22-fold.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+#define MAX_LINE 1000 /* maximum input line length */
+#define LINE_BREAK 4 /* output line length limit */
+
+int getLine(char inputLine[]);
+
+/* Exercise 1-22. Write a program to "fold" long input lines into two or more shorter lines after
+the last non-blank character that occurs before the n-th column of input. Make sure your
+program does something intelligent with very long lines, and if there are no blanks or tabs
+before the specified column. */
+
+/* Test Cases
+ * - (with LINE_BREAK set to 4)
+ * - all inputs & outputs end with \n
+ * - no input can contain \n in the middle
+ * "\n" -> "\n"
+ * "12345678\n" -> "123-\n456-\n78\n"
+ * "1234      \n" -> "1234\n"
+ * "123 456" -> "123\n456\n"
+ * "123  456\n" -> "123\n 456\n"
+ * "12 345" -> "12\n345\n"
+ */
+int main() {
+    char inputLine[MAX_LINE]; /* current input line */
+    int length;
+    while ((length = getLine(inputLine)) > 0) {
+        printf("length: %d\n", length);
+    }
+    return 0;
+}
+
+int getLine(char inputLine[]) {
+    int c = 0;
+    int i = 0;
+    for (i = 0; i < MAX_LINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        inputLine[i] = (char) c;
+    if (c == '\n') {
+        inputLine[i] = (char) c;
+        ++i;
+    }
+    inputLine[i] = '\0';
+    return i;
+}
